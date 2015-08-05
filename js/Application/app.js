2f1e3eefcf00db2e656bfb70c6637c1cc301fd39
Sashko <borshch.sashko@gmail.com>
2015-08-05 13:49:28 +0300

    add ripple effect

--- js/Application/app.js
+++ js/Application/app.js
@@ -11,5 +11,25 @@ define(['jquery'], function($) {
       $('body').toggleClass('noScroll', bool);
       $('.popup').toggleClass('visible', bool);
     });
+
+    var ink, d, x, y;
+    $(".nav-item").click(function(e){
+      if($(this).find(".ink").length === 0){
+        $(this).prepend("<span class='ink'></span>");
+      }
+
+      ink = $(this).find(".ink");
+      ink.removeClass("animate");
+
+      if(!ink.height() && !ink.width()){
+        d = Math.max($(this).outerWidth(), $(this).outerHeight());
+        ink.css({height: d, width: d});
+      }
+
+      x = e.pageX - $(this).offset().left - ink.width()/2;
+      y = e.pageY - $(this).offset().top - ink.height()/2;
+
+      ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+    });
   });
 });
